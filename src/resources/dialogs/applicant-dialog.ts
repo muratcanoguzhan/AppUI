4fa05b2c8666d4a7e316b8a0468d066636a828ff
muratcanoguzhan <m.c.ogzhan@gmail.com>
2021-01-11 02:19:24 +0300

    custom error added: CountryOfOriginError

--- src/resources/dialogs/applicant-dialog.ts
+++ src/resources/dialogs/applicant-dialog.ts
@@ -1,11 +1,20 @@
+import { I18N } from 'aurelia-i18n';
 import { DialogController } from 'aurelia-dialog';
 
 export class ApplicantDialog {
-    static inject = [DialogController];
+    static inject = [DialogController, I18N];
     errors = [];
-    constructor(private controller: DialogController) {
+    constructor(private controller: DialogController, private i18n: I18N) {
     }
     activate(obj) {
-        Object.keys(obj).map(x => this.errors.push(x +": "+ obj[x]));
+        Object.keys(obj).map(x => {
+            debugger;
+            if (x == 'CountryOfOrigin') {
+                let eMsg = this.i18n.tr("errorMessages.CountryOfOriginError");
+                this.errors.push(eMsg);
+            } else {
+                this.errors.push(x + ": " + obj[x]);
+            }
+        });
     }
 }
